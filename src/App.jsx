fd65ee30b44d3c1e935de677f622d1d9f0bee84f
sazib07 <sazibmd93@gmail.com>
2025-11-11 00:39:21 +0600

    pf bg

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
-import React from "react";
-import Header from "./Components/Header";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Header from './Components/Header'
 
-function App() {
-  return (
-    <div className="min-h-screen bg-[#11071F] text-white">
-      <Header />
-      
-      <main className="p-6">
-        <h1 className="text-3xl font-bold">Welcome to My Website 💜</h1>
-        <p className="mt-4 text-gray-300">
-          This is your dark UI with Tailwind background color #11071F.
-        </p>
-      </main>
-    </div>
-  );
-}
 
-export default App;
+const App = () => (
+  <>
+    <Header/>
+    <Banner />
+    <Clients />
+    <Products />
+    <SliderComponent/>
+    <Login />
+    <Footer />
+  </>
+)
+export default App
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
 
 
-// import { LiaSearchDollarSolid } from "react-icons/lia";
-// import { RiDeleteBinLine } from "react-icons/ri";
-// import { FaUserTie } from "react-icons/fa6";
-// import { FaBarsStaggered } from "react-icons/fa6";
+
 
 
 const Header = () => {
@@ -33,31 +30,9 @@ const Header = () => {
     ]
     
     return (
-        <header className="bg-[#11071F] text-white">
-  <nav>
-    <Container>
-      <Flex className="items-center justify-between mt-[20px]">
-        <img className="w-[200px] lg:w-auto" src="images/Logo.png" alt="logo" />
-        <ul className="hidden lg:flex lg:gap-10">
-          {navItems.map((item) => (
-            <ListItem key={item.name} className="nav_item" item={item} />
-          ))}
-        </ul>
-
-        <div className="flex gap-[10px] lg:gap-[33px]">
-          <LiaSearchDollarSolid size={30} className="text-white" />
-          <div className="relative text-white after:content-[''] after:absolute after:top-[-13px] after:right-[-5px] after:w-[17px] after:h-[17px] after:rounded-full after:bg-red-500">
-            <RiDeleteBinLine size={30} />
-          </div>
-          <FaUserTie size={30} className="text-white" />
-        </div>
-
-        <FaBarsStaggered className="lg:hidden text-white" />
-      </Flex>
-    </Container>
-  </nav>
-</header>
-
+        <header>
+         
+        </header>
     )
 }
 
