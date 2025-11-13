daa0aafbe140a07a74ed1927fc28fe4b3821c166
sazib07 <sazibmd93@gmail.com>
2025-11-13 23:28:09 +0600

    flexing

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -3,20 +3,20 @@ import React from 'react'
 const Intro = () => {
   return (
     <div className="flex">
-<div className="div">
+<div className="col col-lg-6">
 
       {/* <div className="c" */}
    <h1 className="text-lg text-[24px] text-teal-500">Hey,This is Sazib Hossain.</h1>
-   <h2 className="text-lg text-[40px]">I'm MERN Stack Developer</h2>
+   <h2 className="text-lg text-[40px] ">I'm MERN Stack Developer</h2>
      
-     <p>A self-taught UI/UX designer, functioning in the industry for 3+ years now.
+     <p className="w-[600px]">A self-taught UI/UX designer, functioning in the industry for 3+ years now.
 I make meaningful and delightful digital products that create an equilibrium
 between user needs and business goals.</p>
 </div>
     
-<div className="div">
+<div className="col col-lg-6">
     <img 
-    className="h-20 w-40"
+    className="h-40 w-80"
      src="logo.png.png" alt="" />
 
 </div>
