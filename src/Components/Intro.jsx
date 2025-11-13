acc72d441335af8cdd2227b023a2593fcd2b9176
sazib07 <sazibmd93@gmail.com>
2025-11-13 23:20:23 +0600

    separation of divs

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 const Intro = () => {
   return (
     <div>
+<div className="div">
 
       {/* <div className="c" */}
    <h1 className="text-lg text-[24px] text-teal-500">Hey,This is Sazib Hossain.</h1>
@@ -11,6 +12,7 @@ const Intro = () => {
      <p>A self-taught UI/UX designer, functioning in the industry for 3+ years now.
 I make meaningful and delightful digital products that create an equilibrium
 between user needs and business goals.</p>
+</div>
     
 
     <img src="logo.png.png" alt="" />
