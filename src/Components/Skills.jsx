00e3aae863fb162da3370171e34bbc4b51013f0a
sazib07 <sazibmd93@gmail.com>
2025-11-22 21:34:38 +0600

    icons positioned

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,20 +1,21 @@
 import React from "react";
 import { FaReact, FaNodeJs, FaDatabase } from "react-icons/fa";
 import { SiMongodb, SiExpress, SiJavascript, SiTailwindcss } from "react-icons/si";
-import { RiNextjsFill, RiBootstrapFill } from "react-icons/ri";
+import { RiNextjsFill, RiBootstrapFill,SiFirebase } from "react-icons/ri";
 import Container from "./container";
 
 export default function Skills() {
   const skills = [
-    { name: "JavaScript (ES6+)", icon: <SiJavascript size={30} /> },
-    { name: "React.js", icon: <FaReact size={30} /> },
-    { name: "Node.js", icon: <FaNodeJs size={30} /> },
+    { name: "JavaScript (ES6+)", icon: <SiJavascript className="text-yellow-400" size={30} /> },
+    { name: "React.js", icon: <FaReact className="text-blue-400" size={30} /> },
+    { name: "Node.js", icon: <FaNodeJs className="text-green-400" size={30} /> },
     { name: "Express.js", icon: <SiExpress size={30} /> },
-    { name: "MongoDB", icon: <SiMongodb size={30} /> },
+    { name: "MongoDB", icon: <SiMongodb className="text-green-500" size={30} /> },
     { name: "REST API", icon: <FaDatabase size={30} /> },
-    { name: "TailwindCSS", icon: <SiTailwindcss size={30} /> },
+    { name: "TailwindCSS", icon: <SiTailwindcss className="text-blue-700" size={30} /> },
     {name : "Next js" ,icon:<RiNextjsFill size={30}/>},
-    {name: "Bootstrap",icon:<RiBootstrapFill size={30}/>},
+    {name: "Bootstrap",icon:<RiBootstrapFill className="text-purple-600" size={30}/>},
+    {name:"firebase",icon:<SiFirebase className="text-yellow-400" size={30} />}
   ];
 
   return (
@@ -31,6 +32,8 @@ export default function Skills() {
           >
             <div className="mb-2 ">{skill.icon}</div>
             <p className="font-medium text-gray-700">{skill.name}</p>
+
+
           </div>
         ))}
       </div>
