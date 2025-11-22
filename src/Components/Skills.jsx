dccfd5e0b34bbcab39478ed47e75a7e85e61ae0e
sazib07 <sazibmd93@gmail.com>
2025-11-22 22:15:16 +0600

    project added

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -10,9 +10,9 @@ export default function Skills() {
     { name: "JavaScript (ES6+)", icon: <SiJavascript className="text-yellow-400" size={30}/>},
     { name: "React.js", icon: <FaReact className="text-blue-400"size={30} /> },
     { name: "Node.js", icon: <FaNodeJs className="text-green-400"size={30}/> },
-    { name: "Express.js", icon: <SiExpress size={30} /> },
+    { name: "Express.js", icon: <SiExpress className="bg-black px-2 py-1 rounded-lg " size={35} /> },
     { name: "MongoDB", icon: <SiMongodb className="text-green-500"size={30}/> },
-    { name: "REST API", icon: <FaDatabase size={30} /> },
+    { name: "REST API", icon: <FaDatabase className="text-blue-400" size={30} /> },
     { name: "TailwindCSS", icon: <SiTailwindcss className="text-blue-700"size={30}/> },
     {name : "Next js" ,icon:<RiNextjsFill size={30}/>},
     {name: "Bootstrap",icon:<RiBootstrapFill className="text-purple-600"size={30}/>},
@@ -27,17 +27,17 @@ export default function Skills() {
   return (
     <Container>
 
-    <div className="w-full  mx-auto p-6">
-      <h2 className="text-3xl font-bold text-center mb-6">My Skills</h2>
+    <div className="w-full mx-auto p-6">
+      <h2 className="text-3xl font-bold text-center mb-6">My Skills & Abilities</h2>
 
-      <div className="grid grid-cols-2 sm:grid-cols-3 gap-6">
+      <div className="grid grid-cols-4 sm:grid-cols-6 gap-6">
         {skills.map((skill, index) => (
           <div
             key={index}
             className="flex flex-col items-center p-4 border rounded-2xl shadow-sm hover:shadow-md transition duration-200 "
           >
             <div className="mb-2 ">{skill.icon}</div>
-            <p className="font-medium text-gray-700">{skill.name}</p>
+            <p className="font-medium text-gray-600">{skill.name}</p>
           </div>
         ))}
       </div>
