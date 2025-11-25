a05eebf26f122f21dce559e7278b97fbb7ce93aa
sazib07 <sazibmd93@gmail.com>
2025-11-25 21:56:54 +0600

    image structure

--- src/Components/Intro.jsx
+++ src/Components/Intro.jsx
@@ -7,7 +7,7 @@ const Intro = () => {
 
     <div className="flex justify-between ">
 
-<div className="col col-lg-6 bg-blue-500">
+<div className="flex-7">
    <h1 className="text-lg text-[24px] text-teal-500">Hey,This is Sazib Hossain.</h1>
    <h2 className="text-lg text-[40px] ">I'm MERN Stack<br/>Developer</h2>
      
@@ -17,13 +17,27 @@ between user needs and business goals.</p>
 
 
 </div>
-    
-<div className="col col-lg-6">
-    <img 
-    className="h-100 mr-[40px]  rounded-[100px] "
-     src="portfolioimg.png.jpg" alt="" />
 
+<div className="flex justify-center items-center p-10">
+  <div className="relative group">
+    
+    {/* Gradient Border */}
+    <div className="absolute -inset-1 bg-gradient-to-r from-purple-500 via-pink-500 to-yellow-500 rounded-3xl blur-lg opacity-60 group-hover:opacity-100 transition"></div>
+
+    {/* Image Container */}
+    <div className="relative bg-white/10 backdrop-blur-xl rounded-3xl p-2 shadow-2xl">
+      <img 
+        src="portfolioimg.png.jpg"
+        alt="Portfolio"
+        className="w-56 h-56 md:w-72 md:h-72 object-cover rounded-2xl shadow-xl transition-all duration-500 group-hover:scale-105"
+      />
+    </div>
+
+  </div>
 </div>
+
+
+
     </div>
 
     </Container>
