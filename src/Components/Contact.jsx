4107284469b8afbd64b9078e68b36de089a58b07
sazib07 <sazibmd93@gmail.com>
2025-11-26 02:08:04 +0600

    contact designed

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -21,7 +21,7 @@ const Contact = () => {
           type="email"
           id="email"
           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-          placeholder="name@flowbite.com"
+          placeholder="@Enter your email"
           required=""
         />
       </div>
@@ -36,7 +36,7 @@ const Contact = () => {
           type="text"
           id="subject"
           className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-          placeholder="Let us know how we can help you"
+          placeholder="Let us know how can i help you"
           required=""
         />
       </div>
@@ -51,13 +51,13 @@ const Contact = () => {
           id="message"
           rows={6}
           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-          placeholder="Leave a comment..."
+          placeholder="Leave a Message..."
           defaultValue={""}
         />
       </div>
       <button
         type="submit"
-        className="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+        className="py-3 px-5 bg-blue-400 text-center text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
       >
         Send message
       </button>
