ef2a2a610a28fb482c51ab3fb2346a3bf7e644fd
sazib07 <sazibmd93@gmail.com>
2026-01-01 01:08:45 +0600

    finsweet add proj

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,5 +1,5 @@
 import projects from "../data/projects";
-import ProjectCard from "./ProjectCard";
+import ProjectCard from "./projectCard";
 
 const Projects = () => {
   return (
--- src/data/projects.js
+++ src/data/projects.js
@@ -2,10 +2,19 @@ const projects = [
   {
     id: 1,
     title: "Saul Design",
-    description: "Creative design portfolio built with HTML, CSS & JS",
+    description: "Creative design portfolio built with HTML, CSS",
     image: "/projects/saul-design.png",
     live: "https://sazib07.github.io/Saul-design/",
     github: "https://github.com/sazib07/Saul-design",
+    tech: ["HTML", "CSS", "JavaScript"]
+  },
+  {
+    id: 1,
+    title: "Finsweet",
+    description: "Creative design portfolio built with HTML, CSS",
+    image: "/projects/saul-design.png",
+    live: "https://sazib07.github.io/finsweet/",
+    github: "https://github.com/sazib07/finsweet/",
     tech: ["HTML", "CSS", "JavaScript"]
   },
   // ...other projects
